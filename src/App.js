2506f071287b0e21596eb91d4d614d90bf9c1a72
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-04-20 11:49:00 -0400

    initaial push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/navBar";
+//import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import{
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <div>
+        <Row style={{margin:"2%"}}>
+          <Col
+            xs={6}
+            lg={6}
+            style={{
+              border: "5px solid red", padding:"20%"
+            }}
+          >
+            {" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
+            aspernatur et alias sed nisi aliquam iusto distinctio dolores enim
+            inventore mollitia cupiditate? Nulla ipsam, quod nihil ea soluta
+            consectetur beatae!{" "}
+          </Col>
+          <Col
+            xs={6}
+            lg={6}
+            style={{
+              border: "5px solid red", padding:"20%"
+            }}
+          >
+            second
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 }
