36e57ac657460e4b18d6b2e31bf0ad8e6713d4fa
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-04-21 12:13:37 -0400

    added styling for app.css

--- src/App.js
+++ src/App.js
@@ -4,28 +4,21 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { Switch, Route } from "react-router-dom";
 import Tab from "./components/tab";
-
+import "./css/App.css"
 import Header from "./components/header";
 import Footer from "./components/footer";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
       <Switch>
         <Route path="/" exact render="" />
         <Route path="/season" render="" />
         <Route path="/state" render="" />
       </Switch>
-      <div
-        style={{
-          margin: "2%",
-          marginBottom: "10%",
-          marginTop: "5%",
-          border: "5px solid red",
-          padding: "5%",
-        }}
-      >
+
+      <NavBar />
+      <div className="app__content">
         <Header />
         <Tab />
       </div>
