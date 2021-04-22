cde7042d16a810d9366d70966c295d180a70a8fd
mpmoran2 <kamokumoran@gmail.com>
2021-04-22 14:10:54 -0700

    Pilahi- Toggle Tab Menu, working on getting it to STAY open or closed

--- src/components/tab.js
+++ src/components/tab.js
@@ -7,6 +7,9 @@ import Nav from "react-bootstrap/Nav";
 import Card from "../components/card";
 import FarmersMarket from './FarmersMarket'
 import Modals from "./Modals";
+import Button from "react-bootstrap/Button"
+
+
 
 const tab = () => {
   return (
@@ -14,32 +17,35 @@ const tab = () => {
       <Tab.Container id="left-tabs-example" defaultActiveKey="first">
         <Row>
           <Col sm={3} lg={2}>
-            <Nav className="flex-column">
-              <Nav.Item>
-                <Nav.Link className="tab__link" eventKey="first">
-                  Home
-                </Nav.Link>
-              </Nav.Item>
-              <hr className="tab__hr" />
-              <Nav.Item>
-                <Nav.Link className="tab__link" eventKey="second">
-                  Change State
-                </Nav.Link>
-              </Nav.Item>
-              <hr className="tab__hr" />
-              <Nav.Item>
-                <Nav.Link className="tab__link" eventKey="third">
-                  Change Month
-                </Nav.Link>
-              </Nav.Item>
-              <hr className="tab__hr" />
-              <Nav.Item>
-                <Nav.Link className="tab__link" eventKey="fourth">
-                  Farmers Market
-                </Nav.Link>
-              </Nav.Item>
-              <hr className="tab__hr" />
-            </Nav>
+          <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">☰ Menu</button>
+          
+              <Nav id="demo" className="flex-column" class="collapse">
+                <Nav.Item>
+                  <Nav.Link className="tab__link" eventKey="first">
+                    Home
+                  </Nav.Link>
+                </Nav.Item>
+                <hr className="tab__hr" />
+                <Nav.Item>
+                  <Nav.Link className="tab__link" eventKey="second">
+                   Change State
+                  </Nav.Link>
+                </Nav.Item>
+                <hr className="tab__hr" />
+                <Nav.Item>
+                  <Nav.Link className="tab__link" eventKey="third">
+                    Change Month
+                  </Nav.Link>
+                </Nav.Item>
+                <hr className="tab__hr" />
+                <Nav.Item>
+                  <Nav.Link className="tab__link" eventKey="fourth">
+                    Farmers Market
+                  </Nav.Link>
+                </Nav.Item>
+                <hr className="tab__hr" />
+              </Nav>
+            
           </Col>
           <Col sm={9}>
             <Tab.Content>
