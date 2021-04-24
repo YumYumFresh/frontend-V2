418c8aa2bcaf0a7883bfc7e40a283bd7fb126629
s-satsangi <s.satsangi@gmail.com>
2021-04-24 01:40:44 -0400

    corrected statesId

--- src/components/stub.js
+++ src/components/stub.js
@@ -2,14 +2,22 @@ import React, { useEffect } from "react";
 import axios from "axios";
 
 const Stub = () => {
-  // useEffect(() => {
-  //   axios
-  //     .get("http://localhost:55327/states/New York/produces?month=October")
-  //     .then((res) => {
-  //       console.log(res);
-  //     });
-  // }, []);
-  //   http://localhost:3000/states/New%20York/produces?month=October&lookupid=31
+  const url = process.env.REACT_APP_BACKEND_URL;
+  useEffect(() => {
+    // axios
+    //   .get(`${url}/states/New%20York/produces?month=October&lookup_id=33`)
+    //   .then((res) => {
+    //     console.log(res);
+    //   });
+    fetch(
+      "http://yumyumfresh.herokuapp.com/states/New%20York/produces?month=October&lookupid=31"
+    )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  // http://localhost:3000/states/New%20York/produces?month=October&lookupid=31
 
   return <div>Stubb (a dub)</div>;
 };
