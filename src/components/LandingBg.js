a9a59a8f87c3fa2b96c28a093e4111b75a199ac5
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-04-30 10:31:38 -0400

    added full background

--- src/components/LandingBg.js
+++ src/components/LandingBg.js
@@ -10,7 +10,7 @@ const LandingBg = () => {
           "url(https://c0.wallpaperflare.com/preview/257/842/322/salad-lettuce-green-salad-iceberg-lettuce.jpg)",
         backgroundRepeat:"no-repeat",
         backgroundSize:"cover",
-        height: "1000px",
+        height: "1080px",
         width: "1920px",
         objectFit: "contain",
         position: "fixed",
