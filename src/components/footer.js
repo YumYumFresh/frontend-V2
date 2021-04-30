a1db1dd9586870ea655ad862f5ed5fa1a8b087c0
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-04-30 14:07:39 -0400

    added display selection mobile and website size

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,28 +1,33 @@
 import React from "react";
-import Navbar from "react-bootstrap/esm/Navbar";
 import "../css/footer.css";
 import { AiOutlineFacebook } from "react-icons/ai";
 import { AiOutlineInstagram } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
 import { FiTwitter } from "react-icons/fi";
-
 
 const Footer = () => {
   return (
     <div className="footer__container">
-      <Navbar expand="lg" variant="dark"> 
-        <Navbar.Brand className="footer__fontLogo" href="/">
+      <div className="footer__div">
+        <span className="footer__fontLogo">
           Yum-Yum Fresh
+          <a
+            href="https://github.com/YumYumFresh"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiFillGithub className="footer__icons" />
+          </a>
           <AiOutlineFacebook className="footer__icons" />
           <AiOutlineInstagram className="footer__icons" />
           <FiTwitter className="footer__icons" />
-        </Navbar.Brand>
-        <a href="https://github.com/YumYumFresh" className="footer__a">
+        </span>
+        <span className="footer__a">
           <h6>Copyright © 2021-Yum-Yum Fresh. All Rights Reserved.</h6>
-        </a>
-      </Navbar>
+        </span>
+      </div>
     </div>
   );
 };
 
 export default Footer;
-
--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,27 @@
 import React from "react";
 import "../css/header.css";
 
-
 const header = () => {
   return (
     <div className="header__container">
-          <div className="header__header">
-            <h1>About</h1>
-          </div>
-          <div className="header__p">
-            <p>
-              {" "}
-              Eating local food while it’s in season is more than just a cool fad for hip youths and hippies alike. It’s a way of life and a
-              necessity to help lead us into creating a sustainable future, for
-              us and for generations to come. Yum Yum Fresh is your go-to site
-              to know not only know what produce is currently in season, but
-              also your resource to find local farmers markets in your area.
-              <hr />
-              Eating seasonally and supporting local farmers not only tastes
-              better, but it puts resources back into your community, creating
-              an eco-system of growth, sustainability, and opportunity.
-            </p>
-          </div>
+      <div className="header__header">
+        <h1>About</h1>
+      </div>
+      <div className="header__p">
+        <p>
+          {" "}
+          Eating local food while it’s in season is more than just a cool fad
+          for hip youths and hippies alike. It’s a way of life and a necessity
+          to help lead us into creating a sustainable future, for us and for
+          generations to come. Yum Yum Fresh is your go-to site to know not only
+          know what produce is currently in season, but also your resource to
+          find local farmers markets in your area.
+          <hr />
+          Eating seasonally and supporting local farmers not only tastes better,
+          but it puts resources back into your community, creating an eco-system
+          of growth, sustainability, and opportunity.
+        </p>
+      </div>
     </div>
   );
 };
