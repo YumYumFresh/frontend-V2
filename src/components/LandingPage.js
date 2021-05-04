009d234c8416fd1502c1a84485b1ec95d4474887
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-05-04 15:58:52 -0400

    added head room for hello fresh on mobile test

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,21 +1,25 @@
 import React from "react";
-import Logo1 from "../images/YYFLogoMain.png";
-// import Logo1 from "../images/realLogo.png";
+import Logo1 from "../images/realLogo.png";
 import { Link } from "react-router-dom";
 import "../css/landingPage.css";
+import Lettuce from "../images/YYFLogoMain.png";
 
 const LandingPage = () => {
   return (
     <>
-      <div className="landingPage__container__mobile">
-        <img src={Logo1} alt="logo" className="landingPage__img" />
+    <div className="landingPage__container__mobile">
+      
+    </div>
+      <div className="landingPage__container">
+        <img src={Lettuce} alt="logo" className="landingPage__img" />
       </div>
       <div className="landingPage__mobile">
-        <div className="landingPage__div">
-          <Link to="/yumyum" className="landingPage__link">
-            Find produce in your location.
-          </Link>
-        </div>
+      <div className="landingPage__div">
+        <Link to="/yumyum" className="landingPage__link">
+          Find produce in your location.
+        </Link>
+      </div>
+
       </div>
     </>
   );
