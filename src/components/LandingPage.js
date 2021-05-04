9ecb3381de979f09322ceac56585be10acc6c989
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-05-04 16:01:05 -0400

    added head room for hello fresh on mobile test

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Logo1 from "../images/realLogo.png";
 import { Link } from "react-router-dom";
 import "../css/landingPage.css";
 import Lettuce from "../images/YYFLogoMain.png";
