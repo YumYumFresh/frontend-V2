ab5337d9c105991f50b238c224086ec5c28c7510
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-05-06 16:09:34 -0400

    landing page flexy and farmers market card zhushing

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,34 +7,36 @@ import Lettuce from "../images/lettuces.png";
 const LandingPage = () => {
   return (
     <>
-      <div 
-        style={{ backgroundImage: `url("${Lettuce}")`,
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        flexWrap: "no-wrap",
-        justifyContent: "center",
-        alignItems: "center",
-       }}>
+      <div
+        style={{
+          backgroundImage: `url("${Lettuce}")`,
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "no-wrap",
+          justifyContent: "center",
+          alignItems: "center",
+        }}>
         {/* <div className="landingPage__container__mobile"></div> */}
         {/* <div className="landingPage__container"> */}
-          <img
-            src={Logo}
-            alt="logo"
-            className="landingPage__img"
-          />
+       <Link to="/yumyum" className="landingPage__link"> <img
+          src={Logo}
+          alt="logo"
+          className="landingPage__img"
+        />
         {/* </div> */}
         {/* <div className="landingPage__mobile"> */}
+        
           <div className="landingPage__div">
-              <Link to="/yumyum" className="landingPage__link">
-              Find produce in your location.
-          </Link>         
+            Find produce in your location
           {/* </div> */}
-        </div>
+          </div>
+        </Link>
+
       </div>
 
     </>
