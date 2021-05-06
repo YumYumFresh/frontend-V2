c189460e40caca39507f5088ba855f3807ca5653
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-05-06 18:19:14 -0400

    responsive when resizing width with @media

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,33 +7,18 @@ import Lettuce from "../images/lettuces.png";
 const LandingPage = () => {
   return (
     <>
-      <div
+      <div className="mainDiv"
         style={{ 
           backgroundImage: `url("${Lettuce}")`,
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          width: "100vw",
-          height: "100vh",
-          display: "flex",
-          flexDirection: "column",
-          flexWrap: "no-wrap",
-          justifyContent: "center",
-          alignItems: "center",
         }}>
-        {/* <div className="landingPage__container__mobile"></div> */}
-        {/* <div className="landingPage__container"> */}
+
        <Link to="/yumyum" className="landingPage__link"> <img
           src={Logo}
           alt="logo"
           className="landingPage__img"
         />
-        {/* </div> */}
-        {/* <div className="landingPage__mobile"> */}
-        
           <div className="landingPage__div">
             Find produce in your location
-          {/* </div> */}
           </div>
         </Link>
 
