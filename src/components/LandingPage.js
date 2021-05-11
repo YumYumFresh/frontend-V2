f526cd426efc15c9407bbe2c941820b59f64da7d
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-05-11 11:32:05 -0400

    hmm, starting over again

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -8,30 +8,21 @@ const LandingPage = () => {
   return (
     <>
       <div className="mainDiv"
-        style={{
+        style={{ 
           backgroundImage: `url("${Lettuce}")`,
         }}>
-        
-        <Link to="/yumyum" className="landingPage__link"> 
-        <img
+
+       <Link to="/yumyum" className="landingPage__link"> 
+       <img
           src={Logo}
           alt="logo"
           className="landingPage__img"
-          style={{
-            borderRadius: "70%", 
-            margin: "50px", 
-            boxShadow: "-10px 22px 30px 2px grey",
-            width: "500px",
-            height: "500px",
-            }}
         />
           <div className="landingPage__div">
-            Find Local Produce
+            Find local produce!
           </div>
         </Link>
-
       </div>
-
     </>
   );
 };
