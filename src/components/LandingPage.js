6b1df67a6069bd9ec2967019252f902ee129b70f
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-05-11 12:02:44 -0400

    replaced logo with round logo

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../css/landingPage.css";
-import Logo from "../images/YYFLogoMain.png";
+import Logo from "../images/YYFRoundLogo.png";
 import Lettuce from "../images/lettuces.png";
 
 const LandingPage = () => {
   return (
     <>
       <div className="mainDiv"
-        style={{ 
+        style={{
           backgroundImage: `url("${Lettuce}")`,
         }}>
 
-       <Link to="/yumyum" className="landingPage__link"> 
-       <img
-          src={Logo}
-          alt="logo"
-          className="landingPage__img"
-        />
+        <Link to="/yumyum" className="landingPage__link">
+          <img
+            src={Logo}
+            alt="logo"
+            className="landingPage__img"
+          />
           <div className="landingPage__div">
             Find local produce!
           </div>
